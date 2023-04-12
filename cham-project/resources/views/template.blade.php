54e7bfb0caff2985071efb90e6cc189b999e409f
pattanapong gagandee <samanwwe222@gmail.com>
2023-04-12 22:26:48 +0700

    add bootstrap file

--- cham-project/resources/views/template.blade.php
+++ cham-project/resources/views/template.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
     <link rel="shortcut icon" href="/img_logo/Chamily_logo_color.png"/>
     <link rel="stylesheet" href="/frontend/stylemenubar.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
     <head>
     <title>Chamily</title>
     <meta charset="UTF-8" />
@@ -9,97 +10,52 @@
     </head>
 
     <body>
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
-      <div class="container">
-          <a class="navbar-brand" href="#">CHAMILY</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
+      <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3 bg-black">
+        <div class="container">
+            <a class="navbar-brand" href="#">CHAMILY</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
 
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <div class="mx-auto"></div>
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link text-white" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="#">About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="#">Schedule</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="#">Wish</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="#">Contact</a>
-              </li>
-            </ul>
-          </div>
-      </div>
-    </nav>
-
-    <!--script src="js/bootstrap.bundle.min.js"></script>
-    <script type="text/javascript">
-      var nav = document.querySelector('nav');
-
-      window.addEventListener('scroll', function () {
-          if (window.pageYOffset > 100) {
-          nav.classList.add('bg-dark', 'shadow');
-          } else {
-          nav.classList.remove('bg-dark', 'shadow');
-          }
-      });
-    </script-->
-      <!-- Banner Image  -->
-      <div class="banner-image w-100 vh-100 d-flex justify-content-center align-items-center">
-          <div class="content text-center">
-              <h1 class="text-white">WEB ZONE</h1>
-          </div>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <div class="mx-auto"></div>
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link text-white" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-white" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-white" href="#">Schedule</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-white" href="#">Wish</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-white" href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+        </div>
+      </nav>
+      dsdsd
+      <div class="content">
+        Wait
       </div>
 
-            <!-- Main Content Area -->
-    <div class="container my-5 d-grid gap-5">
-      <div class="p-5 border">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
-          natus iusto fugit id saepe neque rerum magni laudantium accusantium
-          dolorem numquam quasi.
-        </p>
-      </div>
-      <div class="p-5 border">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
-          natus iusto fugit id saepe neque rerum magni laudantium accusantium
-          dolorem numquam quasi.
-        </p>
-      </div>
-      <div class="p-5 border">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
-          natus iusto fugit id saepe neque rerum magni laudantium accusantium
-          dolorem numquam quasi.
-        </p>
-      </div>
-      <div class="p-5 border">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
-          natus iusto fugit id saepe neque rerum magni laudantium accusantium
-          dolorem numquam quasi.
-        </p>
-      </div>
-      <div class="p-5 border">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
-          natus iusto fugit id saepe neque rerum magni laudantium accusantium
-          dolorem numquam quasi. df
-        </p>
-      </div>
-    </div>
+
+      <script src="/js/bootstrap.bundle.min.js"></script>
+      <script type="text/javascript">
+        var nav = document.querySelector('nav');
+
+        window.addEventListener('scroll', function () {
+            if (window.pageYOffset > 100) {
+            nav.classList.add('bg-dark', 'shadow');
+            } else {
+            nav.classList.remove('bg-dark', 'shadow');
+            }
+        });
+      </script>
     </body>
 </html>
