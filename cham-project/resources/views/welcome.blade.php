e244becb24fd370c1c8efe5373861725c21000d3
pattanapong gagandee <samanwwe222@gmail.com>
2023-04-27 12:06:20 +0700

    finish index page

--- cham-project/resources/views/welcome.blade.php
+++ cham-project/resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.client')
 @section('assets')
     <link rel="stylesheet" href="frontend/style.css">
+    <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 @endsection
 @section('content')
 <div class="" style="background-color: black;">
@@ -25,10 +26,11 @@
         <div class="container d-flex justify-content-center">
             <div class="card" style="width: 90%;background-color: #000;">
                 <div class="card-body" style="text-align: -webkit-center;">
+                    <br>
                     <div class="container">
-                        <h1 style="color:#FFFFFF;" class="text-center"><b>- Chamily Contact -</b></h1>
+                        <h2 style="color:#FFFFFF;" class="text-center"><b>Chamily Contact</b></h2>
                     </div>
-
+                    <br>
                     <div class="card" style="width: 15rem;"><!-- background-color: #FFF4BD; -->
                     <div class="mt-4">
                         <a href="https://line.me/ti/g2/gmYW9Z1ghA44ywx5PbSKXA"><img src="img_qr_chamily/QrCode.jpg" class="img-fluid rounded-start"></a>
@@ -36,12 +38,12 @@
                         <div class="card-body">
                             <h5 class="card-title">Click Me!</h5>
                             <div class="chamily_social">
-                                <a href="#" class="fab fa-facebook"></a>
-                                <a href="#" class="fab fa-twitter"></a>
-                                <a href="#" class="fab fa-instagram"></a>
-                                <a href="#" class="fab fa-tiktok"></a>
-                                <a href="#" class="fab fa-google"></a>
-                                <a href="#" class="fab fa-youtube"></a>
+                                <a href="https://www.facebook.com/ChamilyTH" class="fab fa-facebook media_btn"></a>
+                                <a href="https://twitter.com/Chamily_TH?s=20" class="fab fa-twitter media_btn"></a>
+                                <a href="https://instagram.com/chamily_th?igshid=YmMyMTA2M2Y=" class="fab fa-instagram media_btn"></a>
+                                <a href="https://www.tiktok.com/@chamily_th?_t=8bntwvY6JWb&_r=1" class="fab fa-tiktok media_btn"></a>
+                                <a href="mailto: chamilyth@gmail.com" class="fab fa-google media_btn"></a>
+                                <a href="https://www.youtube.com/channel/UCr-e1cO5ShlsIzzn7AWLV8g" class="fab fa-youtube media_btn"></a>
                             </div>
                         </div>
                     </div>
