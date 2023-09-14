dc8a4e05a8a68ebdcaddb43f1da36797d9d9136e
samanwwe222@gmail.com <Test@testtttwww.com>
2023-09-14 13:00:01 +0700

    dev show page

---
+++ cham-project/public/frontend/shw_card_congrat.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+    <link rel="shortcut icon" href="img_logo/Chamily_logo_color.png"/>
+    <head>
+        <title>Chamily</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+        <link rel="stylesheet" href="frontend/style_card_congrat2.css">
+    </head>
+
+    <nav class="navbar sticky-top bg-dark navbar-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="img_logo/Chamily_logo_color.png" alt="" width="30" height="30" class="d-inline-block align-text-top">
+                &nbsp;Chamily
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Schedule</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Wish</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+
+    <body>
+        <div class="container mt-4 mb-4 d-flex justify-content-center">
+            <div style="text-align: center;">
+                <b>
+                    <h1>Congratulations!</h1>
+                    <div style="text-align: -webkit-center;"><hr></div>                      
+                    <h4>✿ Kodchaporn Leelatheep ✿</h4>
+                </b>
+            </div>
+
+        </div>
+        <div class="wrapper">
+            <div class="card"> <!-- loop show data -->
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum, perspiciatis blanditiis accusamus commodi consectetur id tempora rem iure eligendi quos eos et autem ratione exercitationem earum laborum ad a sequi!</p>
+            </div>
+
+            <div class="card">
+                <div style="text-align: center;">
+                    <b>
+                        <h4>We're always knew you could do it!</h4>
+                        <h4>So proud of you :)</h4>
+                    </b>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
--- cham-project/resources/views/wishing/lip_gloss/show.blade.php
+++ cham-project/resources/views/wishing/lip_gloss/show.blade.php
@@ -1,19 +1,35 @@
 @extends('layouts.client')
 @section('assets')
-<link rel="stylesheet" href="/frontend/style_card_congrat.css?v={{ time() }}">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="/frontend/style_card_congrat2.css?v={{ time() }}">
 @endsection
 @section('content')
+@if(isset($wishing_id) && $wishing_id)
 <div class="container mt-4 mb-4 d-flex justify-content-center">
-    @if(isset($wishing_id) && $wishing_id)
-    <div class="row" style="width: 90%;">
-        <div class="text-center">
-            <h2 style="color: #b68068;"><b>Congratulations!</b></h2>
-        </div>
-        Record : {{count($wishing_lineitems)}}
+    <div style="text-align: center;">
+        <b>
+            <h1>Congratulations!</h1>
+            <div style="text-align: -webkit-center;"><hr></div>                      
+            <h4>✿ Kodchaporn Leelatheep ✿</h4>
+        </b>
     </div>
-    @else
-        ไม่พบแคมเปญ
-    @endif
+
 </div>
+<div class="wrapper">
+    @foreach ($wishing_lineitems as $wishing_lineitem)
+        <div class="card">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum, perspiciatis blanditiis accusamus commodi consectetur id tempora rem iure eligendi quos eos et autem ratione exercitationem earum laborum ad a sequi!</p>
+        </div>
+    @endforeach
+    <div class="card">
+        <div style="text-align: center;">
+            <b>
+                <h4>We're always knew you could do it!</h4>
+                <h4>So proud of you :)</h4>
+            </b>
+        </div>
+    </div>
+</div>
+@else
+    ไม่พบแคมเปญ
+@endif
 @endsection
